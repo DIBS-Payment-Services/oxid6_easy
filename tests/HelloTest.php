fb4a19ab7eb9d75cd8d0804b471dc5b5dadf8b01
stacho <staszekk1212@gmail.com>
2022-05-17 11:31:59 +0200

    Init unit test with coverage.
    Add composer command:
    composer test:coverage-html
    
    This command run test with code coverage and generate raport in HTML

---
+++ tests/HelloTest.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class HelloTest extends TestCase
+{
+    public function testHelloWorld(): void {
+        $this->assertTrue(true);
+    }
+}
